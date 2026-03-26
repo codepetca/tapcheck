d33854790ace177a6f0dd47ed797ec4bc12fe12f
Stewart Chan <4050555+armorup@users.noreply.github.com>
2026-03-26 09:27:35 -0400

    Configure Vercel branch deploys and Convex generated files

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,59 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as api_ from "../api.js";
+import type * as attendance from "../attendance.js";
+import type * as model from "../model.js";
+import type * as rosters from "../rosters.js";
+import type * as server from "../server.js";
+import type * as sessions from "../sessions.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  api: typeof api_;
+  attendance: typeof attendance;
+  model: typeof model;
+  rosters: typeof rosters;
+  server: typeof server;
+  sessions: typeof sessions;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
